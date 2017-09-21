5f88c72ec6f1dc962e526c6dcd35b4f7aa5b0f64
wzshiming <wzshiming@foxmail.com>
2017-09-21 11:27:01 +0800

    优化内存储存

--- memory.go
+++ memory.go
@@ -7,10 +7,18 @@ import (
 	"github.com/wzshiming/task"
 )
 
+type Node struct {
+	n *task.Node
+	d interface{}
+	t time.Time
+}
+
 type Memory struct {
 	m sync.Map
 	t *task.Task
 }
+
+var _ Cache = (*Memory)(nil)
 
 func NewMemory() *Memory {
 	return &Memory{
@@ -20,19 +28,51 @@ func NewMemory() *Memory {
 }
 
 func (m *Memory) Get(key string) interface{} {
-	i, _ := m.m.Load(key)
-	return i
+	i, ok := m.m.Load(key)
+	if !ok {
+		return nil
+	}
+	n, ok := i.(*Node)
+	if !ok {
+		return nil
+	}
+	return n.d
 }
 
 func (m *Memory) Put(key string, val interface{}, timeout time.Duration) error {
-	m.m.Store(key, val)
-	m.t.Add(time.Now().Add(timeout), func() {
-		m.m.Delete(key)
+	err := m.Delete(key)
+	if err != nil {
+		return err
+	}
+
+	if timeout < 0 {
+		timeout = 0
+	}
+
+	now := time.Now()
+	m.m.Store(key, &Node{
+		t: now,
+		d: val,
+		n: m.t.Add(now.Add(timeout), func() {
+			m.m.Delete(key)
+		}),
 	})
 	return nil
 }
 
 func (m *Memory) Delete(key string) error {
+	i, ok := m.m.Load(key)
+	if !ok {
+		return nil
+	}
+	n, ok := i.(*Node)
+	if !ok {
+		m.m.Delete(key)
+		return nil
+	}
+	if n.n != nil {
+		m.t.Cancel(n.n)
+	}
 	m.m.Delete(key)
 	return nil
 }
